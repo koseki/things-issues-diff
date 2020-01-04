09c60f371b7840ac0ceb46bf7827269a1c995dde
KOSEKI Kengo <koseki@gmail.com>
2020-01-04 16:03:27 +0900

    Initial import

---
+++ lib/things/issues/diff.rb
@@ -0,0 +1,9 @@
+require "things/issues/diff/version"
+
+module Things
+  module Issues
+    module Diff
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/things/issues/diff/version.rb
@@ -0,0 +1,7 @@
+module Things
+  module Issues
+    module Diff
+      VERSION = "0.1.0"
+    end
+  end
+end
